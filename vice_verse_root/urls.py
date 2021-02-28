ab0126bb1172b71c17ea420b93679ba82d347180
Pose1don666 <roma.domaratsky@gmail.com>
2021-02-28 21:31:18 +0200

    added function for reversing user text

--- vice_verse_root/urls.py
+++ vice_verse_root/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.reverse_text),
+    path('', views.reverse_text,),
+    path('reverse/', views.reverse, name='reverse'),
+
 ]
--- vice_verse_root/views.py
+++ vice_verse_root/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def reverse_text(request):
     return render(request, 'text_area.html')
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
